2b48316e06a0b2759047d0b26191f23b7367ef83
RaghunathGopavaram <ragopavaram@altiostar.com>
2023-06-12 12:02:34 +0530

    updated extension script

--- add_ext.sh
+++ add_ext.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 branch_name=$(git symbolic-ref --short HEAD)
-for file in *; do
-  mv "$file" "${file}_$branch_name"
+for file in *.py; do
+  filename="${file%.*}"  # Remove the extension
+  extension="${file##*.}"  # Extract the extension
+  new_filename="${filename%_$branch_name}"  # Remove existing branch name, if any
+  mv -f "$file" "${new_filename}_$branch_name.$extension" >/dev/null 2>&1
 done
+
